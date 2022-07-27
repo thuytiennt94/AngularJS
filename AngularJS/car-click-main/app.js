5b0cfdddf74486f05a38b8a3a52f78ad851dd05d
thuytiennt94 <thuytiennt449@gmail.com>
2022-07-27 19:54:00 +0700

    angujs

---
+++ AngularJS/car-click-main/app.js
@@ -0,0 +1,13 @@
+//Model = data(du lieu) --- view = ui(giao dien) --- Controller = brain
+
+//model
+const model = {
+    currentCar : null,
+    cars: [
+        {
+            clickCount: 0,
+            name: 'Coupe Maserati',
+            imgSrc: 'img/'
+        }
+    ]
+}
