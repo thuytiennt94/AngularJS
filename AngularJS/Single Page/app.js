89b1b91db02314e03edfc11922e2161de60ee9e9
thuytiennt94 <thuytiennt449@gmail.com>
2022-08-05 11:00:45 +0700

    angular

---
+++ AngularJS/Single Page/app.js
@@ -0,0 +1,28 @@
+var app = angular.module('myApp', ['ngRoute']);
+
+app.config(function ($routeProvider){
+    $routeProvider
+       .when('/', {
+           templateUrl : 'pages/home.html',
+           controller: 'HomeController'
+       })
+       .when('/blog', {
+           templateUrl : 'pages/blog.html',
+           controller: 'BlogController'
+       })
+       .when('/about', {
+           templateUrl : 'pages/about.html',
+           controller: 'AboutController'
+       })
+       .otherwise({redirectTo: '/'});
+});
+
+app.controller('HomeController', function ($scope){
+    $scope.message = 'Đây là nội dung được gửi từ HomeController';
+});
+app.controller('BlogController', function ($scope){
+    $scope.message = 'Đây là nội dung được gửi từ BlogController';
+});
+app.controller('AboutController', function ($scope){
+    $scope.message = 'Đây là nội dung được gửi từ AboutController';
+});
--- AngularJS/car-click-main/app.js
+++ AngularJS/car-click-main/app.js
@@ -2,12 +2,72 @@
 
 //model
 const model = {
-    currentCar : null,
-    cars: [
-        {
+    currentCar: null,
+    cars: [{
             clickCount: 0,
             name: 'Coupe Maserati',
-            imgSrc: 'img/'
-        }
+            imgSrc: 'img/mec.jpg',
+        },
+        {
+            clickCount: 0,
+            name: 'Camaro SS 1LE',
+            imgSrc: 'img/',
+        },
+        {
+            clickCount: 0,
+            name: 'Dodger charger 1970',
+            imgSrc: 'img/images.jpg',
+        },
+        {
+            clickCount: 0,
+            name: 'Camaro SS 1LE',
+            imgSrc: 'img/images.jpg',
+        },
     ]
+}
+
+const controller = {
+    init() {
+        model.currentCar = model.cars[0];
+
+        //tell our views tialize
+        carListView.init();
+        carView.init();
+    },
+
+    getCurrentCar() {
+
+    },
+    getCars() {
+        return model.cars;
+    },
+}
+
+
+const carListView = {
+    init() {
+        this.carListView = document.getElementById('car-list');
+        this.render();
+    },
+    render() {
+        let car;
+        let elem;
+        let i;
+        const cars = controller.getCars();
+
+        this.carListView.innerHTML = '';
+
+        for (let i = 0; i < cars.length; i++) {
+            car = cars[i];
+
+            elem = document.createElement('li');
+            elem.className = 'list-group-item -flex justify-content-between lh-condensed';
+            elem.style.cursor = 'pointer';
+            elem.textContent = car.name;
+            elem.addEventListener(
+                'click',
+                (function(carCopy: clickCount:number, imgSrc: string, name: string,)),
+            )
+        }
+    }
 }
